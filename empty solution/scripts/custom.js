486c846ff3d30ff2f950d11dd3b4cb4d43ded9f0
David Muolo <davidmuolo1@gmail.com>
2021-01-19 17:53:09 -0600

    submit

---
+++ empty solution/scripts/custom.js
@@ -0,0 +1,23 @@
+﻿/// <reference path="jquery-3.0.0.min.js" />
+
+function openSideNav() {
+    document.getElementById('side-nav-btn').style.display = "none";
+    document.getElementById('classes-nav').style.width = "200px";
+}
+
+function closeSideNav() {
+    document.getElementById('side-nav-btn').style.display = "block";
+    document.getElementById('classes-nav').style.width = "0";
+}
+
+$(function () {
+    $('.thumb').on('click', function () {
+    var imgSrc = $(this).attr("src");
+    $('#lightbox-content').html('<img src="' + imgSrc + '" class=img-responsive"/>');
+    $('#lightbox-container').fadeIn(500);
+    $('#lightbox-container').on('click', function () {
+        $(this).fadeOut(500);
+
+    });
+});
+});
